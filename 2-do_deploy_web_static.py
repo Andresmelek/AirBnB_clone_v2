781da2e46adb6f50176d3a62ff0c7d6ec4a87fad
Andresmelek <candres.isaza@gmail.com>
2020-04-10 12:30:52 -0500

    me la suda

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -47,6 +47,7 @@ def do_deploy(archive_path):
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(shortname))
         print("New version deployed!")
+        return True
 
     except Exception:
         return False
