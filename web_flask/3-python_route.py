f93afb17356bfcaa7c66bba4b1005225db839fca
andresmelek <candres.isaza@gmail.com>
2020-04-21 17:34:02 -0500

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ returns hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """ returns hbnb """
+    return ('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    """ c is fun for craps """
+    complement = text.replace("_", " ")
+    return 'C %s' % complement
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def show_python_is_cool(text):
+    """ python is likeable """
+    complement = text.replace("_", " ")
+    return 'Python %s' % complement
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
