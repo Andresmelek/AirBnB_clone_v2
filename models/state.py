9e4245ae2e932050f0d47802e4ceaf85a188d7fe
andresmelek <candres.isaza@gmail.com>
2020-04-21 19:05:37 -0500

    trying

--- models/state.py
+++ models/state.py
@@ -2,31 +2,27 @@
 """This is the state class"""
 import models
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    __tablename__ = 'states'
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    __tablename__ = "states"
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade="delete")
+        cities = relationship("City", cascade="delete", backref="state")
     else:
         name = ""
-
         @property
         def cities(self):
-            """getter cities"""
-            city = models.storage.all(City)
-            relation_list = []
-            for key in city.values():
-                if key.states.id == self.id:
-                    relation.append(key)
-            return relation_list
+            """returns a list of city"""
+            all_cities = models.storage.all(City)
+            list_cities = []
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
